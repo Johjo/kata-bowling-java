a3b04c43a57a4a0c671f07829791da58f27ef13c
Jonathan LAURENT <johjo@ytreza.org>
2022-05-02 14:32:46 +0200

    red - play game

--- src/TestBowling.java
+++ src/TestBowling.java
@@ -6,6 +6,32 @@ public class TestBowling {
     @Test
     public void one_game() {
         Bowling game = new Bowling();
+
+        game.roll(3);
+        game.roll(4);
+        game.roll(6);
+        game.roll(3);
+        game.roll(2);
+        game.roll(8);
+        game.roll(4);
+        game.roll(3);
+        game.roll(4);
+        game.roll(4);
+        game.roll(5);
+        game.roll(2);
+        game.roll(6);
+        game.roll(4);
+        game.roll(10);
+        game.roll(8);
+        game.roll(0);
+        game.roll(10);
+        game.roll(8);
+        game.roll(1);
+
+
+
+
+
     }
 
 }
