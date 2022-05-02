f76f50ee1ce1ff4977870d145703e02647612ef4
Jonathan LAURENT <johjo@ytreza.org>
2022-05-02 14:54:37 +0200

    refactoring - show duplication of code in frame index

--- src/bowling/Bowling.java
+++ src/bowling/Bowling.java
@@ -20,52 +20,52 @@ public class Bowling {
         score += rolls.get(frameIndex + 1); // roll 2
 
         // frame 2
-        frameIndex = 0 + 2;
+        frameIndex = frameIndex + 2;
         score += rolls.get(frameIndex); // roll 3
         score += rolls.get(frameIndex + 1); // roll 4
 
         // frame 3
-        frameIndex = 2 + 2;
+        frameIndex = frameIndex + 2;
         score += rolls.get(frameIndex); // roll 5
         score += rolls.get(frameIndex + 1); // roll 6 (spare)
         score += rolls.get(frameIndex + 2); // roll 7 (spare bonus)
 
         // frame 4
-        frameIndex = 4 + 2;
+        frameIndex = frameIndex + 2;
         score += rolls.get(frameIndex); // roll 7
         score += rolls.get(frameIndex + 1); // roll 8
 
         // frame 5
-        frameIndex = 6 + 2;
+        frameIndex = frameIndex + 2;
         score += rolls.get(frameIndex); // roll 9
         score += rolls.get(frameIndex + 1); // roll 10
 
         // frame 6
-        frameIndex = 8 + 2;
+        frameIndex = frameIndex + 2;
         score += rolls.get(frameIndex); // roll 11
         score += rolls.get(frameIndex + 1); // roll 12
 
         // frame 7
-        frameIndex = 10 + 2;
+        frameIndex = frameIndex + 2;
         score += rolls.get(frameIndex); // roll 13
         score += rolls.get(frameIndex + 1); // roll 14
         score += rolls.get(frameIndex + 2); // roll 15
 
         // frame 8
-        frameIndex = 12 + 2;
+        frameIndex = frameIndex + 2;
         score += rolls.get(frameIndex); // roll 15 (strike)
         score += rolls.get(frameIndex + 1); // roll 16 (strike bonus)
         score += rolls.get(frameIndex + 2); // roll 17 (strike bonus)
 
 
         // frame 9
-        frameIndex = 14 + 1;
+        frameIndex = frameIndex + 1;
         score += rolls.get(frameIndex); // roll 16
         score += rolls.get(frameIndex + 1); // roll 17
 
 
         // frame 10
-        frameIndex = 15 + 2;
+        frameIndex = frameIndex + 2;
         score += rolls.get(frameIndex); // roll 18 (strike)
         score += rolls.get(frameIndex + 1); // roll 19 (strike bonus)
         score += rolls.get(frameIndex + 2); // roll 20 (strike bonus)
