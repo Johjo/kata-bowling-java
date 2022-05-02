a437b9543202bf82abfcee1ca13f309a13760130
Jonathan LAURENT <johjo@ytreza.org>
2022-05-02 14:40:25 +0200

    refactoring - introduce implicitely concept of frame

--- src/bowling/Bowling.java
+++ src/bowling/Bowling.java
@@ -8,27 +8,48 @@ public class Bowling {
     public int score() {
         int score = 0;
 
+        // frame 1
         score += 3;
         score += 4;
+
+        // frame 2
         score += 6;
         score += 3;
+
+        // frame 3
         score += 2;
         score += 8;
         score += 4;
+
+        // frame 4
         score += 4;
         score += 3;
+
+        // frame 5
         score += 4;
         score += 4;
+
+        // frame 6
         score += 5;
         score += 2;
+
+        // frame 7
         score += 6;
         score += 4;
         score += 10;
+
+        // frame 8
         score += 10;
         score += 8;
         score += 0;
+
+
+        // frame 9
         score += 8;
         score += 0;
+
+
+        // frame 10
         score += 10;
         score += 8;
         score += 1;
