61d50c63db27258f4217329bf4dfdb4a4877c1c8
Jonathan LAURENT <johjo@ytreza.org>
2022-05-02 14:35:42 +0200

    refactoring - explain score calculation

--- src/bowling/Bowling.java
+++ src/bowling/Bowling.java
@@ -6,6 +6,6 @@ public class Bowling {
     }
 
     public int score() {
-        return 117;
+        return 3 + 4 + 6 + 3 + 2 + 8 + 4 +4 + 3 + 4 + 4 + 5 + 2 + 6 + 4 + 10 +10 + 8 + 0 + 8 + 0 + 10 + 8 + 1;
     }
 }
